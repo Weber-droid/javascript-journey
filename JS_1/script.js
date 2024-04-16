c272b15c04a29420c0773a6cc01bbdc5781e274e
Emmanuel Tetteh Totimeh <79781042+Weber-droid@users.noreply.github.com>
2024-04-16 14:12:42 +0000

    Update script.js

--- JS_1/script.js
+++ JS_1/script.js
@@ -1,1 +1,9 @@
+const heading_one = document.querySelector('#heading-one')
 
+function clickme() {
+    const name = prompt("What is your name?")
+    alert(`Welcome ${name}, nice meeting you.`)
+    heading_one.textContent = `Welcome ${name}.`
+
+
+}
