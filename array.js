df8d074d8eb207456e0e8252659e5994de57823d
Emmanuel Tetteh Totimeh <79781042+Weber-droid@users.noreply.github.com>
2024-04-17 23:35:55 +0000

    Create array.js

---
+++ array.js
@@ -0,0 +1,5 @@
+const cars = ["Toyota", "Benz", "Jeep", "BMW"]
+console.log(cars[0]) //Outputs to the console Toyota since it's the first word in the array.
+
+cars[1] = "Parky"
+console.log(cars[1]) // Parky
