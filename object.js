fe84e8a6d5a1e8a704f839139754e8fb97f8e3bd
Emmanuel Tetteh Totimeh <79781042+Weber-droid@users.noreply.github.com>
2024-04-17 23:30:57 +0000

    Create object.js

---
+++ object.js
@@ -0,0 +1,11 @@
+//objects in Javascript
+
+let Person = {
+  name = 'Emma',
+  age = 23,
+  course = 'DSA'
+}
+
+console.log(Person.name) //Emma
+console.log(Person.age) //23
+console.log(Person.course) //DSA
